8ba66a70dc6a339c987ab0ab40b14dbf938dfa24
Bryan <bmayer@fredhutch.org>
2022-04-05 21:31:07 -0700

    updated neut/vl processing

--- data-scripts/pull-shared-data.R
+++ data-scripts/pull-shared-data.R
@@ -4,19 +4,6 @@ library(here)
 
 source(here("R", "directory-funs.R"))
 
-
-# -------- neut data ---------------
-#this is locally stored but has some mild processing
-
-ic_cols = c("gmt50ms", "gmt80ms", "gmt50ls", "gmt80ls", "gmt50mf", "gmt80mf")
-
-read_csv(raw_data_here("AMP Data for Modeling files/amp_survival_wk80_tau_neut_blinded.csv"),
-         col_types = cols(), guess_max = 5000) %>%
-  dplyr::filter(!is.na(nisolates) & nisolates > 0) %>%
-  select(pub_id, nisolates, contains("gmt")) %>%
-  mutate(across(all_of(ic_cols), parse_number)) %>%
-  write_csv(clean_data_here("amp-neut-blinded.csv"))
-
 # -------- rx data ------------------
 
 rx_dat = read_sas("/Volumes/trials/vaccine/p703/analysis/NONMEM/case_control/data/raw/rx_v2.sas7bdat") %>%
@@ -48,5 +35,5 @@ stopifnot(n_distinct(pk_key$pub_id) == n_distinct(pk_data$ID))
 
 pk_data %>%
   left_join(pk_key, by = "ID") %>%
-  write_csv(clean_data_here("pk-data.csv")) 
+  write_csv(clean_data_here("pk-nm-data.csv")) 
 
